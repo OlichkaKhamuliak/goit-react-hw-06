774f72b31703e6f5394d361984ec5085cbe42cc4
OlichkaKhamuliak <olkoboyko97@gmail.com>
2024-02-28 09:22:54 +0200

    added a match check name or number + toaster

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -5,9 +5,11 @@ import css from "./ContactForm.module.css";
 import { nanoid } from "nanoid";
 import { IoPersonAdd } from "react-icons/io5";
 import { IMaskInput } from "react-imask";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addContact } from "../../redux/contactSlice";
 import countries from "./countries";
+import { selectContacts } from "../../redux/selectors";
+import toast, { Toaster } from "react-hot-toast";
 
 const userSchema = Yup.object().shape({
   name: Yup.string()
@@ -20,6 +22,7 @@ const userSchema = Yup.object().shape({
 
 export const ContactForm = () => {
   const dispatch = useDispatch();
+  const contacts = useSelector(selectContacts);
 
   const [countryCode, setCountryCode] = useState("+38"); // Початковий код країни
   const [placeholder, setPlaceholder] = useState("+38 (000)-000-0000");
@@ -39,71 +42,109 @@ export const ContactForm = () => {
   const numberFieldId = useId();
 
   return (
-    <Formik
-      initialValues={{
-        name: "",
-        number: "",
-      }}
-      validationSchema={userSchema}
-      onSubmit={(values, { resetForm }) => {
-        dispatch(addContact({ id: nanoid(), ...values }));
-        resetForm();
-      }}
-    >
-      <Form className={css.form} autoComplete="off">
-        <div className={css.formGroup}>
-          <label className={css.label} htmlFor={nameFieldId}>
-            Name
-          </label>
-          <Field
-            className={css.input}
-            type="text"
-            name="name"
-            id={nameFieldId}
-            placeholder="Name Surname"
-          />
-          <ErrorMessage className={css.error} name="name" component="span" />
-        </div>
+    <div>
+      <Formik
+        initialValues={{
+          name: "",
+          number: "",
+        }}
+        validationSchema={userSchema}
+        onSubmit={(values, { resetForm }) => {
+          //Перевіряємо чи номер або ім'я контакту вже існує в книзі контактів
+          const nameMatch = contacts.find(
+            (contact) =>
+              contact.name.toLowerCase() === values.name.toLocaleLowerCase()
+          );
+          const numberMatch = contacts.find(
+            (contact) =>
+              contact.number.toLowerCase() === values.number.toLocaleLowerCase()
+          );
 
-        <div className={css.formGroup}>
-          <label
-            className={`${css.label} ${css.number}`}
-            htmlFor={numberFieldId}
-          >
-            Number
-          </label>
-          <select
-            className={css.select}
-            value={countryCode}
-            onChange={handleCountryChange}
-          >
-            {countryOptions.map((option) => (
-              <option key={option.value} value={option.value}>
-                {option.label}
-              </option>
-            ))}
-          </select>
-          <Field
-            as={IMaskInput}
-            className={css.input}
-            type="tel"
-            name="number"
-            id={numberFieldId}
-            placeholder={
-              countryCode === "another" ? "Enter number" : placeholder
-            }
-            mask={
-              countryOptions.find((option) => option.value === countryCode).mask
-            }
-          />
-          <ErrorMessage className={css.error} name="number" component="span" />
-        </div>
+          if (nameMatch || numberMatch) {
+            resetForm();
+            const conflictReason =
+              nameMatch && numberMatch
+                ? `name '${values.name}' and phone number '${values.number}'`
+                : nameMatch
+                ? `name '${values.name}'`
+                : `phone number '${values.number}'`;
 
-        <button className={css.button} type="submit">
-          <IoPersonAdd className={css.btnIcon} size="25" />
-          Add user
-        </button>
-      </Form>
-    </Formik>
+            toast.error(
+              `A contact ${conflictReason} is already exist in the address book.`
+            );
+          }
+
+          dispatch(addContact({ id: nanoid(), ...values }));
+          resetForm();
+        }}
+      >
+        <Form className={css.form} autoComplete="off">
+          <div className={css.formGroup}>
+            <label className={css.label} htmlFor={nameFieldId}>
+              Name
+            </label>
+            <Field
+              className={css.input}
+              type="text"
+              name="name"
+              id={nameFieldId}
+              placeholder="Name Surname"
+            />
+            <ErrorMessage className={css.error} name="name" component="span" />
+          </div>
+
+          <div className={css.formGroup}>
+            <label
+              className={`${css.label} ${css.number}`}
+              htmlFor={numberFieldId}
+            >
+              Number
+            </label>
+            <select
+              className={css.select}
+              value={countryCode}
+              onChange={handleCountryChange}
+            >
+              {countryOptions.map((option) => (
+                <option key={option.value} value={option.value}>
+                  {option.label}
+                </option>
+              ))}
+            </select>
+            <Field
+              as={IMaskInput}
+              className={css.input}
+              type="tel"
+              name="number"
+              id={numberFieldId}
+              placeholder={
+                countryCode === "another" ? "Enter number" : placeholder
+              }
+              mask={
+                countryOptions.find((option) => option.value === countryCode)
+                  .mask
+              }
+            />
+            <ErrorMessage
+              className={css.error}
+              name="number"
+              component="span"
+            />
+          </div>
+
+          <button className={css.button} type="submit">
+            <IoPersonAdd className={css.btnIcon} size="25" />
+            Add user
+          </button>
+        </Form>
+      </Formik>
+      <Toaster
+        position="top-right"
+        reverseOrder={false}
+        containerStyle={{
+          top: 80,
+        }}
+      />
+    </div>
   );
 };
